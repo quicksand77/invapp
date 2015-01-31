d3bde93d7750c9f4caaae0c4d5ee862613d6792e
quicksand77 <quicksand77@mail.com>
2015-01-31 13:28:35 -0600

    modified main.py draw, added some frames

--- main.py
+++ main.py
@@ -8,61 +8,74 @@ class InvForm:
         self.root = Tk()
         self.root.title("Inventory Application Form")
         #add/edit radio buttons
+
         self.add = Radiobutton(self.root,indicatoron=False,state=NORMAL,text = "add server",width=16,value = '1',variable=1)
         self.add.grid(row=0, column = 0)
         self.edit = Radiobutton(self.root,indicatoron=False,text = "edit server",width=16,value = '2',variable=1)
         self.edit.grid(row=0, column = 1)
         self.add.select()
         #entry labels/entries
-        self.numHDDlabel = Label(self.root, text = "Number of HDDs:").grid(row=1,column=0)
-        self.numHDDentry = Entry(self.root)
-        self.numHDDentry.grid(row=1, column = 1)
-        self.sizeHDDlabel = Label(self.root, text = "Size of HDDs:").grid(row=2,column=0)
-        self.sizeHDDentry = Entry(self.root)
-        self.sizeHDDentry.grid(row=2, column = 1)
-        self.memLabel = Label(self.root, text = "Memory:").grid(row=3,column=0)
-        self.memEntry = Entry(self.root)
-        self.memEntry.grid(row=3, column = 1)
-        self.numProcLabel = Label(self.root, text = "Processors:").grid(row=4,column=0)
-        self.numProcEntry = Entry(self.root)
-        self.numProcEntry.grid(row=4, column = 1)
-        self.IPdracLabel = Label(self.root,text = "DRAC IP address:").grid(row=5,column=0)
-        self.IPdracEntry = Entry(self.root)
-        self.IPdracEntry.grid(row=5,column=1)
-        self.ip1Label = Label(self.root,text = "IP address 1:").grid(row=6,column=0)
-        self.ip1Entry = Entry(self.root)
-        self.ip1Entry.grid(row=6,column=1)
-        self.ip2Label = Label(self.root,text = "IP address 2:").grid(row=7,column=0)
-        self.ip2Entry = Entry(self.root)
-        self.ip2Entry.grid(row=7,column=1)
-        self.serviceTagLabel = Label(self.root,text="Service Tag:").grid(row=8,column=0)
-        self.serviceTagEntry = Entry(self.root)
-        self.serviceTagEntry.grid(row=8,column=1)
+        self.informationFrame = Frame(bd=3,relief=RIDGE,padx=5,pady=5)
+        # self.informationFrame.config(bg="gray")
+        self.informationFrame.grid(row=1,column=0,rowspan=7,columnspan=2)
+        self.drawEntries()
         #description
-        self.descriptionLabel = Label(self.root,text="Description:").grid(row=1,column=2)
-        self.descriptionEntry = Entry(self.root)
+        self.descriptionFrame = Frame(bd=3,relief=RIDGE)
+        self.descriptionFrame.grid(row=1,column=2,rowspan=7,sticky=N+S)
+        self.descriptionLabel = Label(self.descriptionFrame,text="Description:").grid(row=1,column=2)
+        self.descriptionEntry = Entry(self.descriptionFrame)
         self.descriptionEntry.grid(row=2,column=2,rowspan=7,sticky=N+S)
         self.descriptionEntry.insert(0,"needs formatting/wordwrap")
         #bottom buttons
-        self.closeButton = Button(self.root, text = "Exit", command=self.closeFunc,width=16).grid(row=10, column = 2)
-        self.applyButton = Button(self.root,text="Apply",command=self.applyFunc,width=16).grid(row=10,column=1)
-        self.clearButton = Button(self.root,text="Clear",command=self.clearFunc,width=16).grid(row=10,column=0)
+        self.bottomButtonFrame = Frame(bd=3,relief=RIDGE,padx=3,pady=3)
+        self.bottomButtonFrame.grid(row=10,column=0,columnspan=3)
+        self.closeButton = Button(self.bottomButtonFrame, text = "Exit", command=self.closeFunc,width=16).grid(row=0, column = 2)
+        self.applyButton = Button(self.bottomButtonFrame,text="Apply",command=self.applyFunc,width=16).grid(row=0,column=1)
+        self.clearButton = Button(self.bottomButtonFrame,text="Clear",command=self.clearFunc,width=16).grid(row=0,column=0)
         #drop down list
-        self.var = StringVar(self.root)
-        self.var.set('Server Model')
+        self.serverString = StringVar(self.root)
+        self.serverString.set('Server Model')
         serverModels = ['R610','R710','R720','R620','R630','R730XD']
         self.choices = serverModels
         # I think you can put all 3 lines on one line, more readable (JN)
-        self.option = OptionMenu(self.root,self.var,*self.choices).grid(row=0, column=2, columnspan=2)
-        # self.option.config(width=13)
-        # self.option.grid(row=0,column=2)
+        self.serverOption = OptionMenu(self.root,self.serverString,*self.choices);self.serverOption.config(width=13);self.serverOption.grid(row=0, column=2, columnspan=2)
+
+        #firmware dropdown list
+        self.firmwareString = StringVar(self.root)
+        self.firmwareString.set('Firmware version')
+        self.firmwareOption = OptionMenu(self.root,self.firmwareString,self.firmwareList());self.firmwareOption.config(width=13);self.firmwareOption.grid()
 
         self.root.mainloop()
+    def drawEntries(self):
+        self.numHDDlabel = Label(self.informationFrame, text = "Number of HDDs:").grid(row=0,column=0)
+        self.numHDDentry = Entry(self.informationFrame)
+        self.numHDDentry.grid(row=0, column = 1)
+        self.sizeHDDlabel = Label(self.informationFrame, text = "Size of HDDs:").grid(row=1,column=0)
+        self.sizeHDDentry = Entry(self.informationFrame)
+        self.sizeHDDentry.grid(row=1, column = 1)
+        self.memLabel = Label(self.informationFrame, text = "Memory:").grid(row=2,column=0)
+        self.memEntry = Entry(self.informationFrame)
+        self.memEntry.grid(row=2, column = 1)
+        self.numProcLabel = Label(self.informationFrame, text = "Processors:").grid(row=3,column=0)
+        self.numProcEntry = Entry(self.informationFrame)
+        self.numProcEntry.grid(row=3, column = 1)
+        self.IPdracLabel = Label(self.informationFrame,text = "DRAC IP address:").grid(row=4,column=0)
+        self.IPdracEntry = Entry(self.informationFrame)
+        self.IPdracEntry.grid(row=4,column=1)
+        self.ip1Label = Label(self.informationFrame,text = "IP address 1:").grid(row=5,column=0)
+        self.ip1Entry = Entry(self.informationFrame)
+        self.ip1Entry.grid(row=5,column=1)
+        self.ip2Label = Label(self.informationFrame,text = "IP address 2:").grid(row=6,column=0)
+        self.ip2Entry = Entry(self.informationFrame)
+        self.ip2Entry.grid(row=6,column=1)
+        self.serviceTagLabel = Label(self.informationFrame,text="Service Tag:").grid(row=7,column=0)
+        self.serviceTagEntry = Entry(self.informationFrame)
+        self.serviceTagEntry.grid(row=7,column=1)
     def closeFunc(self):
         print "Thank you for using the Inventory Application Form"
         self.root.destroy()
     def applyFunc(self):
-        newTitle = "Server is %s." % self.var.get()
+        newTitle = "Server is %s." % self.serverString.get()
         self.writeNumHDDentry()
         self.writeSizeHDDentry()
         self.writeMemEntry()
@@ -71,7 +84,7 @@ class InvForm:
         self.writeIPaddress1()
         self.writeIPaddress2()
         self.root.title(newTitle)
-        # color = self.var.get()
+        # color = self.serverString.get()
         # self.root['bg'] = color
     def clearFunc(self):
         self.add.select()
@@ -84,7 +97,7 @@ class InvForm:
         self.ip2Entry.delete(0,END)
         self.descriptionEntry.delete(0,END)
         self.serviceTagEntry.delete(0,END)
-        self.var.set('Server Model')
+        self.serverString.set('Server Model')
     def writeNumHDDentry(self):
         try:
             assert int(self.numHDDentry.get())
@@ -137,6 +150,8 @@ class InvForm:
         except Exception as invalid:
             self.ip2Entry.delete(0,END)
             self.ip2Entry.insert(0,"requires ip address")
+    def firmwareList(self):
+        server = self.serverString.get()
     def writeDescriptionEntry(self):
         description = self.descriptionEntry.get()
     def writeServiceTagEntry(self):
@@ -144,9 +159,9 @@ class InvForm:
     def WRITE(self):
         a=1
 
-
-print __name__
 if __name__ == "__main__":
-    print "please launch using blah blah"
-    form = InvForm()
-    form()
+    # print "please launch using blah blah"
+    # def form():
+    #     form = InvForm()
+    # form()
+    InvForm()
