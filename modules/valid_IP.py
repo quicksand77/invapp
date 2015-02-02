f4a30dc467599919bff81c3cc4f76d335b188cc5
jeffnic <password123>
2015-02-02 16:27:35 -0600

    added some code to run standalone for debugging

--- modules/valid_IP.py
+++ modules/valid_IP.py
@@ -8,3 +8,10 @@ class CheckIP:
             return True
         except:
             return False
+
+
+# added as a sanity check (JN)
+if __name__ == "__main__":
+    localCheck = CheckIP()
+    testIP = raw_input("please enter an IP address for me to check: \n")
+    print localCheck.checkip(testIP)
